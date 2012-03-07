016f005d3524b09b3ea301dfedba6e8af45a36ec
Paul Stadig <paul@stadig.name>
2012-03-07 09:04:52 +0000

    Don't AOT.  It causes compatibility errors.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject org.clojars.pjstadig/chinese-democracy "1.2.0"
   :description "master election protocol"
-  :dependencies [[org.clojure/clojure "1.2.1"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [commons-codec "1.4"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]]
-  :main chinese.democracy)
+  :dev-dependencies [[swank-clojure "1.2.1"]])
